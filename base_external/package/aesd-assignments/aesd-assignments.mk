8be3235bcb467c3872aa106d5adffdb5265a7429
UnawareITry <krishnendu.marathe@gmail.com>
2026-07-14 10:41:56 +0530

    add dropbear in the config

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '7117b0430b458092b5ae134fa497b42d49dddc8b'
+AESD_ASSIGNMENTS_VERSION = 'b786529a1a76bfc89b556ccb707a31a44c7bd691'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
