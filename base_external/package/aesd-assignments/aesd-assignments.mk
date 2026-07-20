87f43b0805107e6a345cd2523bbfd226f885a1fc
UnawareITry <krishnendu.marathe@gmail.com>
2026-07-20 16:01:29 +0530

    local host and qemu issue fixes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5f63b06f22d6aa2cae96f16c98176fd93ec1ad15'
+AESD_ASSIGNMENTS_VERSION = 'ce25123b24f909055c193132116822667599bf95'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
